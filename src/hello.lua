b1e6d048a2f1b2d37089cb89cfa454092706f206
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2022-07-06 18:47:45 -0300

    add more lua simple code examples

--- src/hello.lua
+++ src/hello.lua
@@ -3,3 +3,6 @@ print("Hello World")
 print 'Hello, World!'
 
 -- this is a coment in lua
+
+print(10*10)
+print("We have multiplied 10 by 10")
--- src/tables.lua
+++ src/tables.lua
@@ -1,1 +1,15 @@
-a_table = {} -- Creates a new, empty table
+ExampleTable =
+{
+  {1, 2, 3, 4},
+  {5, 6, 7, 8}
+}
+print(ExampleTable[1][3]) -- Prints "3"
+print(ExampleTable[2][4]) -- Prints "8"
+
+function Point(x, y)        -- "Point" object constructor
+    return { x = x, y = y }   -- Creates and returns a new object (table)
+end
+
+array = { Point(10, 20), Point(30, 40), Point(50, 60) }   -- Creates array of points
+-- array = { { x = 10, y = 20 }, { x = 30, y = 40 }, { x = 50, y = 60 } };
+print(array[2].y)
